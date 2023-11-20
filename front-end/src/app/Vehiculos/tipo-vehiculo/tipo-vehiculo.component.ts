860360d29badeb21dd7cc68a895de693a9cf87d0
JulioMolina14 <respaldo863@gmail.com>
2023-11-20 10:14:02 -0500

    NGmodel Tipovehiculo

--- front-end/src/app/Vehiculos/tipo-vehiculo/tipo-vehiculo.component.ts
+++ front-end/src/app/Vehiculos/tipo-vehiculo/tipo-vehiculo.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import { TipoVehiculoService } from './services/tipo-vehiculo.service';
+import { TipoVehiculo } from '../Interfaces/vehiculos.interface';
 
 @Component({
   selector: 'app-tipo-vehiculo',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./tipo-vehiculo.component.css']
 })
 export class TipoVehiculoComponent {
+  constructor(private ServicioTipoVehiculo:TipoVehiculoService){}
+
+  @Input() TipoVehiculos:TipoVehiculo = {
+    Id: 0,
+    Nombre: '',
+    Disponible: ''
+  }
+
+  ConsultarTipoVehiculo(){
+    console.log(this.ServicioTipoVehiculo.TipoVehiculo)
+  }
+
+  RegistrarTipoVehiculo(){
+
+    this.ConsultarTipoVehiculo()
+    console.log(this.TipoVehiculos)
+
+   if (this.TipoVehiculos.Nombre=='' && this.TipoVehiculos.Id==0){
+    return;
+
+    
+   }
+   
+
+    this.ServicioTipoVehiculo.RegistrarTipoVehiculo(this.TipoVehiculos)
+    .subscribe(resp =>{
+     console.log(resp);
+    });
+} 
 
 }
